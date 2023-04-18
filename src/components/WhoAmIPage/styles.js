9300ad3f1937fe4fb9b1c49374924a669b3fa599
akane52 <lulomo@bemen3.cat>
2023-04-18 20:22:48 +0200

    WhoAmI Page almost done

--- src/components/WhoAmIPage/styles.js
+++ src/components/WhoAmIPage/styles.js
@@ -4,7 +4,10 @@ export const FirstPageContainer = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 10%;
-    margin-left: 10%;
+`
+export const WhoAmITitleAndText = styled.div`
+    margin-left: 40%;
+    margin-bottom: 10%;
 `
 export const IntroductionTitle = styled.div`
     font-size: 2.5em;
@@ -12,9 +15,14 @@ export const IntroductionTitle = styled.div`
     color: #654AFF;
     margin-bottom: 1%;
 `
-
 export const IntroductionText = styled.div`
     font-size: 1.5em;
     width: 85%;
     text-align: justify;
+`
+export const LuciaPhoto = styled.img`
+    width: 20%;
+    height: 40%;
+    margin-top: -40%;
+    margin-left: 10%;
 `
