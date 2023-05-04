1f9918cce2db530c1fc4d19c79bd7187686f1816
akane52 <lulomo@bemen3.cat>
2023-05-04 18:35:14 +0200

    personalObjectivesCards done with a .map element

--- src/components/PersonalObjectivesCards/index.jsx
+++ src/components/PersonalObjectivesCards/index.jsx
@@ -8,76 +8,23 @@ import {
     CirclePersonalObjectivesCard,
     Logos
 } from "./styles"
-import codingPhoto from "../../resources/images/coding_logo.png"
-import grouth from "../../resources/images/grouth.png"
-import contactsLogo from "../../resources/images/contacts.png"
-import error_logo from "../../resources/images/error_logo.png"
-import ui_logo from "../../resources/images/ui_logo.png"
+
+import { personalObjectivesData } from '../../dataSamples/PersonalObjectivesInfo';
 
 const PersonalObjectivesCard = () => {
   return (
     <CardContainer>
-        <CardBackground>
-            <CirclePersonalObjectivesCard>
-                <Logos src={codingPhoto} />            
-            </CirclePersonalObjectivesCard>
-            <CardTextContainer>
-            <CardTitle>Nuevas tecnologías</CardTitle>
-            <CardDescription>
-                Aprender una nueva tecnología o lenguaje de programación 
-                relacionado con el desarrollo web para mejorar la capacidad 
-                técnica y el conocimiento.
-            </CardDescription>
-            </CardTextContainer>
+      {personalObjectivesData.map((data) => (
+        <CardBackground key={data.id}>
+          <CirclePersonalObjectivesCard>
+            <Logos src={data.logo} />            
+          </CirclePersonalObjectivesCard>
+          <CardTextContainer>
+            <CardTitle>{data.title}</CardTitle>
+            <CardDescription>{data.description}</CardDescription>
+          </CardTextContainer>
         </CardBackground>
-        <CardBackground>
-            <CirclePersonalObjectivesCard>
-                <Logos src={grouth} />            
-            </CirclePersonalObjectivesCard>
-            <CardTextContainer>
-            <CardTitle>Crecimiento personal</CardTitle>
-            <CardDescription>Uno de mis principales objetivos personales ha sido siempre crecer tanto a 
-                nivel profesional como personal. Y considero que estas prácticas me han dado la oportunidad
-                de hacerlo.
-            </CardDescription>
-            </CardTextContainer>
-        </CardBackground>
-        <CardBackground>
-            <CirclePersonalObjectivesCard>
-                <Logos src={contactsLogo} />            
-            </CirclePersonalObjectivesCard>
-            <CardTextContainer>
-            <CardTitle>Red de contactos</CardTitle>
-            <CardDescription>
-                Establecer contactos profesionales y mantener relaciones de 
-                trabajo sólidas con compañeros de trabajo para construir una red de contactos
-            </CardDescription>
-            </CardTextContainer>
-        </CardBackground>
-        <CardBackground>
-            <CirclePersonalObjectivesCard>
-                <Logos src={ui_logo} />            
-            </CirclePersonalObjectivesCard>
-            <CardTextContainer>
-            <CardTitle>UX/UI</CardTitle>
-            <CardDescription>
-                Desarrollar habilidades en diseño de interfaz de usuario (UI) y 
-                experiencia de usuario (UX) para crear aplicaciones atractivas y fáciles de usar.
-            </CardDescription>
-            </CardTextContainer>
-        </CardBackground>
-        <CardBackground>
-            <CirclePersonalObjectivesCard>
-                <Logos src={error_logo} />            
-            </CirclePersonalObjectivesCard>
-            <CardTextContainer>
-            <CardTitle>Resolución de problemas</CardTitle>
-            <CardDescription>
-                Desarrollar habilidades de resolución de problemas, para poder identificar y
-                solucionar errores y problemas en el código.
-            </CardDescription>
-            </CardTextContainer>
-        </CardBackground>
+      ))}
     </CardContainer>
   )
 }
---
+++ src/dataSamples/PersonalObjectivesInfo.js
@@ -0,0 +1,38 @@
+import codingPhoto from "../../src/resources/images/coding_logo.png"
+import grouth from "../../src/resources/images/grouth.png"
+import contactsLogo from "../../src/resources/images/contacts.png"
+import error_logo from "../../src/resources/images/error_logo.png"
+import ui_logo from "../../src/resources/images/ui_logo.png"
+
+export const personalObjectivesData = [
+    {
+      id: 1,
+      logo: codingPhoto,
+      title: "Nuevas tecnologías",
+      description: "Aprender una nueva tecnología o lenguaje de programación relacionado con el desarrollo web para mejorar la capacidad técnica y el conocimiento."
+    },
+    {
+      id: 2,
+      logo: grouth,
+      title: "Crecimiento personal",
+      description: "Uno de mis principales objetivos personales ha sido siempre crecer tanto a nivel profesional como personal. Y considero que estas prácticas me han dado la oportunidad de hacerlo."
+    },
+    {
+      id: 3,
+      logo: contactsLogo,
+      title: "Red de contactos",
+      description: "Establecer contactos profesionales y mantener relaciones de trabajo sólidas con compañeros de trabajo para construir una red de contactos"
+    },
+    {
+      id: 4,
+      logo: ui_logo,
+      title: "UX/UI",
+      description: "Desarrollar habilidades en diseño de interfaz de usuario (UI) y experiencia de usuario (UX) para crear aplicaciones atractivas y fáciles de usar."
+    },
+    {
+      id: 5,
+      logo: error_logo,
+      title: "Resolución de problemas",
+      description: "Desarrollar habilidades de resolución de problemas, para poder identificar y solucionar errores y problemas en el código."
+    }
+  ];
