0f2b77f122dc22e9de605baa91475453f93aea3c
akane52 <lulomo@bemen3.cat>
2023-05-04 19:01:49 +0200

    Timeline shortened

---
+++ src/dataSamples/TimelineInfo.js
@@ -0,0 +1,98 @@
+import gherking from "../../src/resources/images/gherking_logo.png"
+import javascript from "../../src/resources/images/javascript_logo.jpg"
+import bdd from "../../src/resources/images/bdd_logo.png"
+import react from "../../src/resources/images/react_logo.png"
+import brain from "../../src/resources/images/brain_logo.png"
+import travelport from "../../src/resources/images/travelport_logo_redondo.png"
+import grupo_trabajo from "../../src/resources/images/grupo_trabajo.png"
+
+export const timelineData = [
+    {
+      id: 1,
+      logo: gherking,
+      date: "JUNIO 2022",
+      subtitle: "Gherking",
+      info: "Creación de una “wikipedia” con toda la parte teórica de HTML, CSS. Utilización de github",
+      learnings: "He aprendido a utilizar el lenguaje de marcas html y las hojas de estilo css a nivel avanzado. Aprendizaje de github a nivel de usuario básico."
+    },
+    {
+        id: 2,
+        logo: javascript,
+        date: "JULIO 2022",
+        subtitle: "JavaScript",
+        info: "Comienzo con JavaScript. Creación de una calculadora en base a un feature file y realización de los tests automáticos. (gherkin)",
+        learnings: "He aprendido a utilizar el lenguaje de programación JavaScript a un nivel avanzado. También a realizar feature files y a generar tests automáticos para comprobar el funcionamiento de la aplicación."
+      },
+      {
+        id: 3,
+        logo: javascript,
+        date: "SEPTIEMBRE 2022",
+        subtitle: "JavaScript",
+        info: "Continuación con JavaScript. Creación de un buscaminas con metodología BDD.",
+        learnings: "Hemos seguido con JavaScript. También hemos aprendido a realizar feature files y a generar tests automáticos para comprobar el funcionamiento de la aplicación."
+      },
+      {
+        id: 4,
+        logo: bdd,
+        date: "OCTUBRE 2022",
+        subtitle: "JavaScript y BDD",
+        info: "Continuación con JavaScript. Creación de un buscaminas con metodología BDD.",
+        learnings: "Hemos seguido con JavaScript. También hemos aprendido a realizar feature files y a generar tests automáticos para comprobar el funcionamiento de la aplicación."
+      },
+      {
+        id: 5,
+        logo: react,
+        date: "NOVIEMBRE 2022",
+        subtitle: "React y Emotion",
+        info: "Iniciación a React y Emotion en la réplica de la página web utilizada en la empresa. (Storefront)",
+        learnings: "Hemos aprendido a utilizar el framework de React y la librería Emotion para css. Aparte, hemos aprendido a trabajar en equipo y a organizarnos para llegar a un objetivo común."
+      },
+      {
+        id: 6,
+        logo: react,
+        date: "DICIEMBRE 2022",
+        subtitle: "React y Emotion",
+        info: "Iniciación a React y Emotion en la réplica de la página web utilizada en la empresa. (Storefront)",
+        learnings: "Hemos aprendido a utilizar el framework de React y la librería Emotion para css. Aparte, hemos aprendido a trabajar en equipo y a organizarnos para llegar a un objetivo común."
+      },
+      {
+        id: 7,
+        logo: brain,
+        date: "ENERO 2023",
+        subtitle: "Pruebas de conocimientos",
+        info: "Prueba práctica para poner a prueba nuestros conocimientos sobre React, llamadas a API, Emotion y testing. Creación de una web de temática libre para poner en práctica nuestros conocimientos.",
+        learnings: "Hemos aprendido a gestionarnos en un corto periodo de tiempo para acabar una tarea que nos han pedido. Aparte, hemos tenido que aprender en base a buscar documentación a utilizar APIs y a gestionar sus datos. Al igual que con la prueba práctica, hemos tenido que organizarnos en pequeños grupos para acabar una web completamente funcional en 2 semanas. Todo esto, aplicando los conocimientos anteriormente aprendidos (React, Styled components, deployment, llamadas a las API…)"
+      },
+      {
+        id: 8,
+        logo: travelport,
+        date: "FEBRERO 2023",
+        subtitle: "Introducción a SmartPoint Cloud",
+        info: "Introducción a Smart Point Cloud, haciéndonos un pequeño ejercicio para coger los conocimientos básicos. Una vez acabado esto, nos eligieron a varios para incorporarnos a un equipo de trabajo. ",
+        learnings: "Empezamos ya a tocar el framework de la empresa. Nos hicieron un ejercicio que consistía en poner un botón en una navbar, que al hacer click nos saliese un historial de comandos y que, por último, apareciese un terminal propio de la empresa que al insertar un comando, nos apareciese el historial de comandos utilizados. "
+      },
+      {
+        id: 9,
+        logo: grupo_trabajo,
+        date: "MARZO 2023",
+        subtitle: "Introducción a grupos de trabajo",
+        info: "Empezamos a incorporarnos al equipo haciendo tareas de mantenimiento como por ejemplo actualizar yarn en varios repositorios, también asistiendo a las dailys y a los PI planning para coger el ritmo de trabajo.",
+        learnings: "Organización interna de los equipos, matificación de la metodología scrum, actualización de versiones…"
+      },
+      {
+        id: 10,
+        logo: grupo_trabajo,
+        date: "MARZO 2023",
+        subtitle: "Introducción a grupos de trabajo",
+        info: "Empezamos a incorporarnos al equipo haciendo tareas de mantenimiento como por ejemplo actualizar yarn en varios repositorios, también asistiendo a las dailys y a los PI planning para coger el ritmo de trabajo.",
+        learnings: "Organización interna de los equipos, matificación de la metodología scrum, actualización de versiones…"
+      },
+      {
+        id: 11,
+        logo: grupo_trabajo,
+        date: "ABRIL 2023",
+        subtitle: "Introducción a grupos de trabajo",
+        info: "Empezamos a incorporarnos al equipo haciendo tareas de mantenimiento como por ejemplo actualizar yarn en varios repositorios, también asistiendo a las dailys y a los PI planning para coger el ritmo de trabajo.",
+        learnings: "Organización interna de los equipos, matificación de la metodología scrum, actualización de versiones…"
+      },
+]
